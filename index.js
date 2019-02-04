5e675c85de81980dd49338ca7feb0c13b6d8713c
Samuel Cornet <samuel@noukod.com>
2019-02-04 22:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const returnFirstTwoDrivers = function (drivers) {
 const returnLastTwoDrivers = function (drivers) {
   return drivers.slice(-2);
 }
+const createFareMultiplier = function(int) {
+    return function(value){
+      return int *  value;
+    }    
+}
